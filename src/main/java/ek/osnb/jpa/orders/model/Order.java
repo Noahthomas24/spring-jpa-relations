629c44508c799829961666437a03b360f62c1281
Noahthomas24 <noth0001@stud.kea.dkgit config --global user.email noth0001@stud.kea.dk>
2025-08-29 11:07:48 +0200

    Completed exercise 03

--- src/main/java/ek/osnb/jpa/orders/model/Order.java
+++ src/main/java/ek/osnb/jpa/orders/model/Order.java
@@ -13,7 +13,7 @@ import java.util.List;
 public class Order extends BaseEntity {
 
     @JsonManagedReference
-    @OneToMany(mappedBy = "order")
+    @OneToMany(mappedBy = "order", cascade = {CascadeType.PERSIST, CascadeType.MERGE, CascadeType.REMOVE})
     private List<OrderLine> orderLines = new ArrayList<>();
 
     private LocalDate orderDate;

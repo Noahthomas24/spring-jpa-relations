6313b5877dded17735db593c9a76358663c43624
Noahthomas24 <noth0001@stud.kea.dkgit config --global user.email noth0001@stud.kea.dk>
2025-08-29 11:27:34 +0200

    Completed exercise 04

--- src/main/java/ek/osnb/jpa/orders/model/Order.java
+++ src/main/java/ek/osnb/jpa/orders/model/Order.java
@@ -1,6 +1,5 @@
 package ek.osnb.jpa.orders.model;
 
-import com.fasterxml.jackson.annotation.JsonManagedReference;
 import ek.osnb.jpa.common.model.BaseEntity;
 import jakarta.persistence.*;
 
@@ -12,7 +11,6 @@ import java.util.List;
 @Table(name = "orders")
 public class Order extends BaseEntity {
 
-    @JsonManagedReference
     @OneToMany(mappedBy = "order", cascade = {CascadeType.PERSIST, CascadeType.MERGE, CascadeType.REMOVE})
     private List<OrderLine> orderLines = new ArrayList<>();
 

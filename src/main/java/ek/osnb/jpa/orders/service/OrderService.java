1bb7c9dfee8bbe397e6e037903dcb963653512e6
Noahthomas24 <noth0001@stud.kea.dkgit config --global user.email noth0001@stud.kea.dk>
2025-08-29 11:47:02 +0200

    Completed exercise 05

--- src/main/java/ek/osnb/jpa/orders/service/OrderService.java
+++ src/main/java/ek/osnb/jpa/orders/service/OrderService.java
@@ -2,15 +2,20 @@ package ek.osnb.jpa.orders.service;
 
 
 import ek.osnb.jpa.orders.dto.OrderDto;
+import ek.osnb.jpa.orders.dto.OrderLineDto;
 import ek.osnb.jpa.orders.model.Order;
 import ek.osnb.jpa.orders.model.OrderStatus;
 
 import java.util.List;
 
 public interface OrderService {
+
     List<OrderDto> getAllOrders(OrderStatus status);
     OrderDto getOrderById(Long id);
     OrderDto createOrder(OrderDto orderDto);
     OrderDto updateOrder(Long id, OrderDto orderDto);
     void deleteOrder(Long id);
+    void addOrderLine(Long orderId, OrderLineDto orderLineDto);
+    void removeOrderLine(Long orderId, Long orderLineId);
+
 }
